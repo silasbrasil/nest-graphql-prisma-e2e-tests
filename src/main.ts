ad505aeb813f09c8913167d527ba20ee1640aeea
Silas Brasil <silas.swf@gmail.com>
2022-02-07 16:28:32 -0300

    First commit

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const PORT = Number(process.env.PORT) || 3000;
+
+  const app = await NestFactory.create(AppModule);
+  await app.listen(PORT);
+
+  console.log('Listening on port:', PORT);
+}
+bootstrap();
