cd0b2395290d6b49ad522bcd2c5061f902f7f7bd
Silas Brasil <silas.swf@gmail.com>
2022-02-08 14:40:15 -0300

    test: initial tests

---
+++ src/user/user.service.spec.ts
@@ -0,0 +1,26 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserService } from './user.service';
+
+describe('UserService', () => {
+  let service: UserService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [UserService],
+    }).compile();
+
+    service = module.get<UserService>(UserService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should return a user', async () => {
+    const id = 12;
+
+    const user = await service.getUserById(id);
+
+    expect(id).toBe(id);
+  })
+});
