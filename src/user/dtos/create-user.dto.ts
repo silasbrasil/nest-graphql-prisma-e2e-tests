87d6239b557cf387beea8a0b94f99a847c2eaf91
Silas Brasil <silas.swf@gmail.com>
2022-02-09 10:52:24 -0300

    feature: Created CreateUserDto validations

--- src/user/dtos/create-user.dto.ts
+++ src/user/dtos/create-user.dto.ts
@@ -1,5 +1,11 @@
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 export class CreateUserDto {
+  @IsEmail({}, { message: "O campo email deve ser um email válido" })
   email: string;
+
+  @IsOptional()
+  @IsNotEmpty({ message: "O campo name não pode ser vazio" })
+  @IsString({ message: "O campo name dever uma String" })
   name: string;
 }
